673056eae9a5e21d8d1259634866faa92bb7323d
Matthias <matthias.nowakowski@gmail.com>
2018-07-07 14:35:53 +0200

    Tasks und AllTopics eingerichtet (Layout bei Tasks ist noch verbessungsbedürftig)

--- app/src/main/java/com/example/students/lerntagebuchoop/fragment/Tasks.java
+++ app/src/main/java/com/example/students/lerntagebuchoop/fragment/Tasks.java
@@ -1,14 +1,33 @@
 package com.example.students.lerntagebuchoop.fragment;
 
 import android.content.Context;
+import android.content.res.XmlResourceParser;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.students.lerntagebuchoop.R;
+import com.example.students.lerntagebuchoop.adapter.TaskListAdapter;
+import com.example.students.lerntagebuchoop.model.TaskItem;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.lang.reflect.Field;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,15 +43,26 @@ public class Tasks extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    private String taskName;
+    private List<String> questionList;
+    View mView;
+    String title;
+    ListView mListView;
+    //ArrayAdapter<String> mAdapter;
+    TaskListAdapter mAdapter;
+    ArrayList<TaskItem> taskItems;
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+    private TextView mtitle;
 
     public Tasks() {
         // Required empty public constructor
     }
+
 
     /**
      * Use this factory method to create a new instance of
@@ -59,13 +89,129 @@ public class Tasks extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        if(this.getArguments() == null){
+            try {
+                this.taskName = findCurrentLecture();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            this.taskName = (String) this.getArguments().get("taskName");
+        }
+
+        int id = getResources().getIdentifier(this.taskName, "xml", getContext().getPackageName());
+        XmlPullParserFactory factory = null;
+        try {
+            factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(true);
+            factory.newPullParser();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        }
+
+        taskItems = new ArrayList<>();
+
+        //finde Titel und Fragen für aktuelles Thema
+        XmlPullParser rp = getResources().getXml(id);
+        TaskItem ti = null;
+        try {
+            while (rp.next() != XmlResourceParser.END_DOCUMENT) {
+                switch(rp.getEventType()) {
+                    case XmlResourceParser.START_TAG:
+                        switch (rp.getName()) {
+                            case "description":
+                                ti = new TaskItem();
+                                ti.setDescription(rp.nextText());
+                                break;
+                            case "text":
+                                if(ti != null) {
+                                    ti.setText(rp.nextText());
+                                    taskItems.add(ti);
+                                }
+                                break;
+                            case "topic":
+                            title = rp.nextText();
+                            break;
+                        }
+                        break;
+                }
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String findCurrentLecture() throws IllegalAccessException, ParseException {
+        // Alle XMLs nach DATE-Tag durchsuchen
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.mm.yyyy");
+        String currentLecture = null;
+        Date today = sdf.parse(sdf.format(new Date()));
+        Date currentLectureDate = null;
+        Date tempDate = null;
+
+        XmlPullParserFactory factory = null;
+        try {
+            factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(true);
+            factory.newPullParser();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        }
+
+        Field[] xmlFields = R.xml.class.getFields();
+
+        for(int i=0; i<xmlFields.length; i++){
+            // finde VL mit aktuellem Datum
+            XmlPullParser rp = getResources().getXml(xmlFields[i].getInt(null));
+            try {
+                while (rp.next() != XmlResourceParser.END_DOCUMENT) {
+                    switch (rp.getEventType()) {
+                        case XmlResourceParser.START_TAG:
+                            if("date".equals(rp.getName())){
+                                tempDate = sdf.parse(rp.nextText());
+                                if(i == 0){
+                                    currentLectureDate = tempDate;
+                                    currentLecture = xmlFields[i].getName();
+                                }
+                                else if(tempDate.after(currentLectureDate) &&
+                                        tempDate.before(today)){
+                                    currentLectureDate = tempDate;
+                                    currentLecture = xmlFields[i].getName();
+                                }
+                            }
+                    }
+                }
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return currentLecture;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tasks, container, false);
+
+        mView = inflater.inflate(R.layout.fragment_tasks, container, false);
+        mtitle = (TextView) mView.findViewById(R.id.lectureTitle);
+        mtitle.setText(title);
+        mListView = (ListView) mView.findViewById(R.id.taskList);
+        mAdapter = new TaskListAdapter(getContext(), taskItems);
+        mListView.setAdapter(mAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            }
+        });
+        return mView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
